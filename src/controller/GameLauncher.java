edfaaff7a01592df44b915ab8da2bffcaa4c332e
Benjamin Fausett <benjaminfausett@gmail.com>
2024-09-03 08:58:00 -0400

    removed blackjack

--- src/controller/GameLauncher.java
+++ src/controller/GameLauncher.java
@@ -1,33 +1,23 @@
 package controller;
 
-import model.blackjack.Blackjack;
-import model.common.CardGame;
 import model.crazyeights.CrazyEights;
 import model.gops.Gops;
 import model.war.War;
 import view.TextInterface;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
 
 public class GameLauncher {
 	
 	public void start() {
 		String name = TextInterface.getResponse("Welcome to CardGames! What is your name?");
 		
-		TextInterface.display("Available Games: War, Blackjack, GOPS, Crazy Eights");
+		TextInterface.display("Available Games: War, GOPS, Crazy Eights");
 		
 		while (true) {
-			String response = TextInterface.getValidResponse("What game would you like to play?", "War", "Blackjack", "GOPS", "Crazy Eights", "Quit");
+			String response = TextInterface.getValidResponse("What game would you like to play?", "War", "GOPS", "Crazy Eights", "Quit");
 			
 			switch (response.toUpperCase()) {
 				case "WAR":
 					new War(name).play();
-					break;
-				case "BLACKJACK":
-					new Blackjack(name).play();
 					break;
 				case "GOPS":
 					new Gops(name).play();
