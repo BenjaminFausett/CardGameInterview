58e3116ee107765d6fe422fd104d1b4eef245c06
Benjamin Fausett <benjaminfausett@gmail.com>
2025-04-29 12:33:08 -0400

    cleanup

--- src/model/x/X.java
+++ src/model/x/X.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public class X extends CardGame {
 	
 	public X(String playerName) {
-		throw new UnsupportedOperationException("This method is not yet implemented");
+	
 	}
 	
 	@Override
